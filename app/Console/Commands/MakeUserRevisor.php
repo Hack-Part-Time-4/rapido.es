4ef0f7da68879b3c62df7b205fa608bfe66a4596
Jeiny Fernandez <jeinyenriquefernandez@gmail.com>
2022-12-21 18:40:15 +0100

    MakeUserRevisor

--- app/Console/Commands/MakeUserRevisor.php
+++ app/Console/Commands/MakeUserRevisor.php
@@ -7,24 +7,26 @@ use Illuminate\Console\Command;
 class MakeUserRevisor extends Command
 {
 
-protected $signature = 'rapido:makeUserRevisor';
-protected $description = 'Asigna el rol de revisor a un usuario';
-public function __construct()
-{
-    parent::__construct();
-}
-
-public function handle()
-{
-    $email = $this->ask("Introducir el correo del usuario");
-    $user = User::where('email', $email)->first();
-    if(!$user){
-        $this->error("Usuario no encontrado");
-        return;
+    protected $signature = 'rapido:makeUserRevisor';
+    protected $description = 'Asigna el rol de revisor a un usuario';
+    public function __construct()
+    {
+        parent::__construct();
     }
-    $user->is_revisor = true;
-    $user->save();
-    $this->info("El usuario $user->name ya es un revisor");
+
+    public function handle()
+    {
+        $email = $this->ask("Introducir el correo del usuario");
+        $user = User::where('email', $email)->first();
+        if(!$user){
+            $this->error("Usuario no encontrado");
+            return;
+        }
+        $user->is_revisor = true;
+        $user->save();
+        $this->info("El usuario $user->name ya es un revisor");
+    }
+    
 
     /**
      * The name and signature of the console command.

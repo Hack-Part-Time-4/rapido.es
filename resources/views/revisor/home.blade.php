e226b5bc0287b0ace781aad0bc1e440e1acf5c56
Ivan Calderon <digitalvan18@gmail.com>
2023-01-20 20:17:35 +0100

    arreglado vista revisor

--- resources/views/revisor/home.blade.php
+++ resources/views/revisor/home.blade.php
@@ -2,7 +2,7 @@
     <x-slot name='title'>{{__('Affiliate - Revisor')}}</x-slot>
 
     @if ($ad)
-    <div class='container my-5 py-5 vh-100'>
+    <div class='container my-1 py-5'>
         <div class='row'>
             <div class='col-12 col-md-8 offset-md-2'>
                 <div class="card">
